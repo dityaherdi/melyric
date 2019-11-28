487cfa13581f85f5d0aba7e59d05606c9748a8a9
dityaherdi <dityaherdi@gmail.com>
2019-11-28 13:47:11 +0700

    improve autoscroll functionality

--- src/components/parts/modals/LyricModal.vue
+++ src/components/parts/modals/LyricModal.vue
@@ -9,17 +9,19 @@
                     <img uk-img src="https://i.scdn.co/image/07c323340e03e25a8e5dd5b9a8ec72b69c50089d" alt="No Picture Available">
                 </div>
                 <div class="uk-padding">
-                    <p class="uk-article-meta uk-margin-remove-bottom">Autoscroll</p>
+                    <p class="uk-article-meta uk-margin-remove-bottom">Autoscroll {{ scrollRange }}</p>
                     <ul class="uk-iconnav">
                         <li><a href="javascript:void(0)" @click="scrollActiveSwitcher" :uk-icon="scrollActive ? 'icon: ban' : 'icon: play-circle'"></a></li>
-                        <li><a href="javascript:void(0)" uk-icon="icon: plus-circle"></a></li>
-                        <li><a href="javascript:void(0)" uk-icon="icon: minus-circle"></a></li>
+                        <template v-if="scrollActive">
+                            <li><a href="javascript:void(0)" @click="increaseScrollSpeed" uk-icon="icon: plus-circle"></a></li>
+                            <li><a href="javascript:void(0)" @click="decreaseScrollSpeed" uk-icon="icon: minus-circle"></a></li>
+                        </template>
                     </ul>
                     <article class="uk-article">
                         <h1 class="uk-article-title"><a class="uk-link-reset" href="">Heading</a></h1>
                         <p class="uk-article-meta">Song by <a href="#">Artist</a> in 2012.</p>
                         <div class="overflow-area" id="lyricArea" uk-overflow-auto>
-                        <p style="white-space:pre-wrap;">{{ lyric === null ? '' : lyric }}</p>
+                        <p class="lyric-overflow">{{ lyric === null ? '' : lyric }}</p>
                         </div>
                     </article>
                 </div>
@@ -40,7 +42,9 @@ export default {
         return {
             lyric: null,
             scroll: null,
-            scrollActive: false
+            scrollActive: false,
+            scrollDistance: 25,
+            scrollTime: 2000
         }
     },
     created() {
@@ -58,6 +62,20 @@ export default {
             }
         }
     },
+    computed: {
+        scrollRange: function () {
+            const counter = 250
+            let times = (this.scrollTime / counter) - 8
+            if (times < 0) {
+                let displayNumber = Math.abs(times)
+                return `+${displayNumber}x`
+            } else if (times > 0) {
+                return `-${times}x`
+            } else {
+                return ''
+            }
+        }
+    },
     methods: {
         hideLyricModal: function () {
             this.lyric = null
@@ -65,15 +83,38 @@ export default {
             UIkit.modal('#modal-full').hide();
         },
         startAutoScroll: function () {
+            this.stopAutoScroll()
             this.scroll = setInterval(() => {
-                document.getElementById('lyricArea').scrollBy({ top: 10, left: 0, behavior: 'smooth' })
-            }, 1000)
+                const element = document.getElementById('lyricArea')
+                if (element) {
+                    element.scrollBy({ top: this.scrollDistance, left: 0, behavior: 'smooth' })
+                } else {
+                    return false
+                }
+            }, this.scrollTime)
         },
         stopAutoScroll: function () {
+            this.scrollTime = 2000
             clearInterval(this.scroll)
         },
         scrollActiveSwitcher: function () {
             return this.scrollActive = !this.scrollActive
+        },
+        increaseScrollSpeed: function () {
+            if (this.scrollTime > 0) {
+                this.startAutoScroll()
+                this.scrollTime -= 250
+            } else {
+                console.log('already at +8x speed')
+            }
+        },
+        decreaseScrollSpeed: function () {
+            if (this.scrollTime <= 4000) {
+                this.startAutoScroll()
+                this.scrollTime += 250
+            } else {
+                console.log('already at -8 speed')
+            }
         }
     }
 }
@@ -82,5 +123,9 @@ export default {
 <style scoped lang="less">
 .overflow-area {
     min-height: 50vh !important;
+}
+.lyric-overflow {
+    white-space: pre-wrap;
+    text-align: center;
 }
 </style>
